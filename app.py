2d4d5b6304fd1dae968f8d85690493e5013a2c18
zer0ne1 <nguyenkhanhtung.nkt01@gmail.com>
2022-11-21 09:31:19 +0700

    fix

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-@app.route('/test')
-def hello():
-    with urlopen('http://127.0.0.1:10707//vinorsoft/streamingcamera/v1.0/processManagement/get-path-streaming/?camera_id=cam1&height_resolution=234&width_resolution=416/') as r:
-        text = r.read()
-    return text
-
 @app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
